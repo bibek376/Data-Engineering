df985b55bd18e954a53ebea7955dbdce3f439b4d
bibek376 <cu.18bcs6728@gmail.com>
2023-03-13 12:14:00 +0545

    data ingest

--- Week_1/Data_upload.py
+++ Week_1/Data_upload.py
@@ -1,72 +1,51 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import pandas as pd 
 from sqlalchemy import create_engine
-import argparse
+import pandas as pd 
+from time import time
 
-# user 
-# password
-# host
-# port
-# database name
-# table name
-# url of the csv
-
-def main(params):
-    user=params.user
-    password=params.password
-    host=params.host
-    port=params.port
-    database=params.database
-    table_name=params.table_name
-    url=params.url
-    engine=create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
-    engine.connect()
+engine=create_engine('postgresql://postgres:password@localhost:5000/ny_taxi_db')
+engine.connect()
 
 
 
-#   CSV download
-    csv_name='output.csv'
-    
+df_iter=pd.read_csv('/home/bibek/Desktop/Data_Engineering/Data/yellow_tripdata_2021-01.csv',iterator=True,chunksize=100000)
+df=next(df_iter)
+
+
+df.tpep_pickup_datetime=pd.to_datetime(df.tpep_pickup_datetime)
+df.tpep_dropoff_datetime=pd.to_datetime(df.tpep_dropoff_datetime)
 
 
 
-parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
-
-parser.add_argument('user',help='user name for postgres')
-parser.add_argument('password',help='password for postgres')
-parser.add_argument('host',help='host for postgres')
-parser.add_argument('port',help='port for postgresql')
-parser.add_argument('database',help='database name for postgres')
-parser.add_argument('table_name',help='name of the table where we will write data')
-parser.add_argument('url',help='url of the csv file')
-
-
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+df.head(n=0).to_sql(name='ny_taxi_data',con=engine,if_exists='replace')
 
 
 
+df.to_sql(name='ny_taxi_data',con=engine,if_exists='append')
 
 
-df=pd.read_csv('/home/bibek/Downloads/train.csv')
+#Upload all data in SQL table
+
+while True:
+    try:
+        t_start=time()
+
+        df=next(df_iter)
+
+        df.tpep_pickup_datetime=pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime=pd.to_datetime(df.tpep_dropoff_datetime)
+
+        df.to_sql(name='ny_taxi_data',con=engine,if_exists='append')
+
+        t_end=time()
+
+        print("Inserted another Chunk,took%.3f second" %(t_start-t_end))
+    except StopIteration:
+        print("All data has been processed.")
+        break
 
 
-df.pickup_datetime=pd.to_datetime(df.pickup_datetime)
-df.dropoff_datetime=pd.to_datetime(df.dropoff_datetime)
 
 
-
-df_iter=pd.read_csv('/home/bibek/Downloads/train.csv',iterator=True,chunksize=20000)
-
-
-
-df.pickup_datetime=pd.to_datetime(df.pickup_datetime)
-df.dropoff_datetime=pd.to_datetime(df.dropoff_datetime)
-
-
-
-df.head(n=0).to_sql(name='ny_taxi',con=engine,if_exists='replace')
-
-df.to_sql(name='ny_taxi',con=engine,if_exists='append')
