3b4e57caba2aa9446a4c097a0ed1f4596a98dda5
bibek376 <cu.18bcs6728@gmail.com>
2023-04-10 14:56:17 +0545

    Foreign key

--- Week_1/Query_practice_For_Data_engineering.sql
+++ Week_1/Query_practice_For_Data_engineering.sql
@@ -3,6 +3,8 @@ select * from ny_taxi_data ntd ;
 -- ALTER TABLE zones DROP COLUMN index;
 -- ALTER TABLE ny_taxi_data DROP COLUMN index;
 
+
+---Join 
 SELECT
   t.tpep_pickup_datetime,
   t.tpep_dropoff_datetime,
@@ -15,9 +17,20 @@ join zones zdo on t."DOLocationID" = zdo."LocationID"
 limit 100;
 
 
+--check if pickup and dropoff location (foreign key)
 
+select * from zones z ;
+select * from ny_taxi_data ntd ;
 
-
+SELECT
+  t.tpep_pickup_datetime,
+  t.tpep_dropoff_datetime,
+  t.total_amount
+from ny_taxi_data t
+left join zones zpu on t."PULocationID" = zpu."LocationID"
+left join zones zdo on t."DOLocationID" = zdo."LocationID"
+where zpu."LocationID" is null
+or zdo."LocationID" is null;
 
 
 
