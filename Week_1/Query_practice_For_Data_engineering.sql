20abb736110a4ec06bb6ae49058d26bf76d9b87e
bibek376 <cu.18bcs6728@gmail.com>
2023-04-10 14:47:00 +0545

    Join

--- Week_1/Query_practice_For_Data_engineering.sql
+++ Week_1/Query_practice_For_Data_engineering.sql
@@ -1,1 +1,25 @@
 select * from zones z ;
+select * from ny_taxi_data ntd ;
+-- ALTER TABLE zones DROP COLUMN index;
+-- ALTER TABLE ny_taxi_data DROP COLUMN index;
+
+SELECT
+  t.tpep_pickup_datetime,
+  t.tpep_dropoff_datetime,
+  t.total_amount,
+  CONCAT(zpu."Borough", ' / ', zpu."Zone") As "pickup_loc",
+  CONCAT(zdo."Borough", ' / ' , zpu."Zone") As "dropoff_loc"
+from ny_taxi_data t
+join zones zpu on t."PULocationID" = zpu."LocationID"
+join zones zdo on t."DOLocationID" = zdo."LocationID"
+limit 100;
+
+
+
+
+
+
+
+
+
+
