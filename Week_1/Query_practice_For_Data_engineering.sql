f0789ecd1ffdce839326158e06399096a3e3e884
bibek376 <cu.18bcs6728@gmail.com>
2023-04-10 15:02:11 +0545

    number of trips per day

--- Week_1/Query_practice_For_Data_engineering.sql
+++ Week_1/Query_practice_For_Data_engineering.sql
@@ -39,5 +39,9 @@ select * from zones z ;
 
 delete from zones where "LocationID" =10;
 
-
+--number of trips per day
+select date_trunc('DAY',ntd.tpep_dropoff_datetime),count(*)  
+from ny_taxi_data ntd 
+group by 1
+order by 2 desc;
 
